550dd89333e401848c97865594418f68cde6957a
Chethan Mogaveer <chethanmogaveer143@gmail.com>
2022-01-09 17:31:43 +0530

    edited test file2

--- src/test/java/com/employmanagementsystem/EmployManagementSystemApplicationTests.java
+++ src/test/java/com/employmanagementsystem/EmployManagementSystemApplicationTests.java
@@ -3,8 +3,11 @@ package com.employmanagementsystem;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootConfiguration
 @SpringBootTest
 class EmployManagementSystemApplicationTests {
 
