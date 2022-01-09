0e4834151b7f47340ca7739086bd73dab6e3dc76
Chethan Mogaveer <chethanmogaveer143@gmail.com>
2022-01-09 11:49:55 +0530

    initial commit

---
+++ src/main/java/com/employeemanagementsystem/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.employeemanagementsystem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.employeemanagementsystem.entity.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+	// This will be AUTO IMPLEMENTED by Spring into a Bean
+}
